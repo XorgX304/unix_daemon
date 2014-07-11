069f461f909d6a7bce27adb7752d41fd1dfe66e3
Yoshida Shin <wbcchsyn@gmail.com>
2014-07-11 09:27:59 +0900

    Version up to 1.0.

--- setup.py
+++ setup.py
@@ -21,13 +21,14 @@ from setuptools import setup
 long_description = open('docs/README.rst').read()
 
 classifiers = [
-    "Development Status :: 4 - Beta",
+    "Development Status :: 5 - Production/Stable",
     "Intended Audience :: Developers",
     "License :: OSI Approved :: Apache Software License",
     "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
     "Programming Language :: Python :: 2.6",
     "Programming Language :: Python :: 2.7",
-    "Programming Language :: Python :: 3.2",
+    "Programming Language :: Python :: 3",
     "Programming Language :: Python :: 3.3",
     "Topic :: Software Development",
     "Topic :: Software Development :: Libraries",
@@ -36,7 +37,7 @@ classifiers = [
 
 setup(
     name='unix_daemon',
-    version='0.1.4',
+    version='1.0.0',
     description='Function emulating Daemon(3) on Linux and Unix OS.',
     long_description=long_description,
     author='Yoshida Shin',
